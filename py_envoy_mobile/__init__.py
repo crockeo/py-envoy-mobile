9a5b657d7ffe99a3afb2d90dd143f18dc185260a
Cerek Hillen <chillen@lyft.com>
2020-11-14 04:11:46 -0500

    make an engine from python

--- py_envoy_mobile/__init__.py
+++ py_envoy_mobile/__init__.py
@@ -1,3 +1,4 @@
 from py_envoy_mobile.wrapper import c_types_wrapper
 
-c_types_wrapper.system("ls")  # type: ignore
+# build a envoy mobile engine
+engine = c_types_wrapper.Engine()
--- py_envoy_mobile/wrapper/c_types_wrapper.cc
+++ py_envoy_mobile/wrapper/c_types_wrapper.cc
@@ -1,42 +1,85 @@
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
 
-static PyObject *c_types_wrapper_system(PyObject *self, PyObject *args) {
-  const char *command;
-  int sts;
+#include <iostream>
 
-  if (!PyArg_ParseTuple(args, "s", &command)) {
-    return nullptr;
+#include "library/common/main_interface.h"
+#include "library/common/types/c_types.h"
+
+// PyEngineObject wraps envoy-mobile's envoy_engine_t type into a Python class that creates an engine when it is
+// initialized.
+struct PyEngineObject {
+  PyObject_HEAD
+  envoy_engine_t engine;
+};
+
+static PyObject *PyEngineObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
+  PyEngineObject *self;
+  self = (PyEngineObject *)type->tp_alloc(type, 0);
+  if (self != nullptr) {
+    self->engine = 0;
   }
-
-  sts = system(command);
-  return PyLong_FromLong(sts);
+  return (PyObject *)self;
 }
 
-static PyMethodDef CTypesWrapperMethods[] = {
-  {
-    "system",
-    c_types_wrapper_system,
-    METH_VARARGS,
-    "Execute a shell command",
-  },
-  {nullptr, nullptr, 0, nullptr},
+static int PyEngineObject_init(PyEngineObject *self, PyObject *args, PyObject *kwargs) {
+  envoy_engine_t engine = init_engine();
+  if (engine == 0) {
+    return -1;
+  }
+
+  self->engine = engine;
+  return 0;
+}
+
+static void PyEngineObject_dealloc(PyEngineObject *self) {
+  if (self->engine != 0) {
+    terminate_engine(self->engine);
+  }
+}
+
+static PyTypeObject PyEngineType = {
+  PyVarObject_HEAD_INIT(nullptr, 0)
+
+  .tp_name = "c_types_wrapper.Engine",
+  .tp_doc = "envoy-mobile engine handle",
+  .tp_basicsize = sizeof(PyEngineObject),
+  .tp_itemsize = 0,
+  .tp_flags = Py_TPFLAGS_DEFAULT,
+  .tp_new = PyEngineObject_new,
+  .tp_init = (initproc)PyEngineObject_init,
+  .tp_dealloc = (destructor)PyEngineObject_dealloc,
 };
+
 
 static struct PyModuleDef c_types_wrapper_module = {
   PyModuleDef_HEAD_INIT,
   "c_types_wrapper",
   nullptr,
   -1,
-  CTypesWrapperMethods,
 };
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-PyObject *PyInit_c_types_wrapper(void) {
-  return PyModule_Create(&c_types_wrapper_module);
+PyMODINIT_FUNC
+PyInit_c_types_wrapper(void) {
+  if (PyType_Ready(&PyEngineType) < 0)
+    return nullptr;
+
+  auto mod = PyModule_Create(&c_types_wrapper_module);
+  if (mod == nullptr)
+    return nullptr;
+
+  Py_INCREF(&PyEngineType);
+  if (PyModule_AddObject(mod, "Engine", (PyObject *)&PyEngineType) < 0) {
+    Py_DECREF(&PyEngineType);
+    Py_DECREF(&mod);
+    return nullptr;
+  }
+
+  return mod;
 }
 
 #ifdef __cplusplus
