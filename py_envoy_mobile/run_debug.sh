ceb1d083142c6660f296e30a7791b33fb730581c
Cerek Hillen <chillen@lyft.com>
2020-11-30 09:37:51 -0500

    don't lie in debug script

--- py_envoy_mobile/run_debug.sh
+++ py_envoy_mobile/run_debug.sh
@@ -2,4 +2,14 @@
 
 # this is meant to be executed from within bazel via bazel run //py_envoy_mobile:debug, it starts a
 # gdb or lldb process on python3, depending on which are available
-lldb -- python3 -m py_envoy_mobile.__init__
+if command -v gdb &> /dev/null; then
+    debugger=gdb
+elif command -v lldb &> /dev/null; then
+    debugger=lldb
+else
+    # TODO: allow a user to pass in a debugger
+    echo "ERROR: neither gdb or lldb are available"
+    exit 1
+fi
+
+$debugger -- python3 -m py_envoy_mobile.__init__
